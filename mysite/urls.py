76a0866513149eb00f3fbc7202b67fc956eab0c6
Feihong Hsu <feihong.hsu@gmail.com>
2016-12-03 15:29:17 -0600

    Just use the login and logout views from django.contrib.auth, not everything

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url('^', include('django.contrib.auth.urls')),
+    url('^login/$', auth_views.login, name='login'),
+    url('^logout/$', auth_views.logout, name='logout'),
     url(r'', include('blog.urls')),
 ]
